13c3cf86aa9a340a3ed41f74877862d943f5f52c
aidan3001 <111900104+aidan3001@users.noreply.github.com>
2022-09-12 11:52:35 -0500

    Add files via upload

---
+++ grocery.py
@@ -0,0 +1,3 @@
+name = input("what's your name? ")
+
+print ("Hello " + name + ". Thank you for choosing our store. Here are the available sections. \n Produce, Deli, Bakery, Alcohol, Beverages, Frozen foods, Pharmacy")
