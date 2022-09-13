77ee5c8709f170389b73569330ac70cba876284e
aidan3001 <111900104+aidan3001@users.noreply.github.com>
2022-09-13 13:02:04 -0500

    all items, half of prices

--- grocery.py
+++ grocery.py
@@ -1,3 +1,16 @@
+
 name = input("what's your name? ")
 
 print ("Hello " + name + ". Thank you for choosing our store. Here are the available sections. \n Produce, Deli, Bakery, Alcohol, Beverages, Frozen foods, Pharmacy")
+
+produce = {'lettuce':'$2','apples':'$1.80/lb','oranges':'$2.50/lb','grapes':'$2/lb','watermelon':'$5','mango':'$0.80','celery':'$1.79','bananas':'$0.49/lb','strawberries':'$3/lb','kiwis':'$4/lb'}
+Deli = {'turkey':'$6.50','ham':'$6.50','roast beef':'$6.50','pepperoni':'$5','chicken':'$4/lb','steak':'$14/lb','sandwich':'$5','salad':'$4','cheese':'$3','soup':'$2.70'}
+Bakery = {'bread':'$4','cake':'$6','cupcakes':'$3.50','donuts':'$3','brownies':'$5','pie':'$4','cheesecake':'$4','muffins':'$3','cookies':'$2.50','rolls':'$5'}
+Alcohol = {'wine':'10','whiskey':'$30','beer':'$16','vodka':'$14','tequila':'','bourbon':'','sake':'','champagne':'','seltzers':'','sparkling water':''}
+Beverages = {'rootbeer':'','sprite':'','monster':'','redbull':'','fanta':'','coke':'','dr pepper':'','pepsi':'','mtn dew':'','coffee':''}
+Frozen = {'ice cream':'','pizza':'','fries':'','breakfast sandwich':'','vegetables':'','pizza rolls':'','waffles':'','fish':'','chicken':'','hash browns':''}
+Pharmacy = {'advil':'','tylenol':'','icyhot':'','vitamins':'','protein':'','toothpaste':'','deodorant':'','shampoo':'','soap':'','wipes':''}
+
+sec = input("Please enter a section. ")
+
+if sec =    
